fc4c31be0009d1408d842d893336c3bc0b4b5a7b
nguyendat1234 <47876667+nguyendat1234@users.noreply.github.com>
2020-04-16 07:17:39 +0700

    make it better

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ customer: customer.id
 });
  
 // app listening on port 3000
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 console.log('server is running on port 3000');
 });
